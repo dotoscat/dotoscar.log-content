3c7ab258cfe116a389c62d84ded54a4c264b37ed
Oscar Triano dotoscat <user@example.com>
2019-07-17 21:15:47 +0200

    Reemplazar simbolos {} por parentesis como simbolos de sustitucion

--- content/pages/sobremi.md
+++ content/pages/sobremi.md
@@ -2,4 +2,4 @@ title: Sobre mí
 date: 4/7/2019
 script: sobre-mi.js
 
-Me llamo Óscar Triano García <span id="nacimiento" data-fechanacimiento="1987">y tengo {} años.<span><noscript>y nací en el año 1987.</noscript>.
+Me llamo Óscar Triano García <span id="nacimiento" data-fechanacimiento="1987">y tengo (mmmm...) años<span><noscript>y nací en el año 1987</noscript>.
--- mi-tema/static/js/sobre-mi.js
+++ mi-tema/static/js/sobre-mi.js
@@ -4,5 +4,5 @@ window.addEventListener('load', evt => {
     const nacimiento = document.getElementById('nacimiento');
     const fechaNacimiento = nacimiento.dataset.fechanacimiento;
     const edad = Math.floor((Date.now() - new Date(fechaNacimiento)) / 1000 / 60 / 60 / 24 / 365);
-    nacimiento.innerText = nacimiento.innerText.replace('{}', edad);
+    nacimiento.innerText = nacimiento.innerText.replace(/\(.*?\)/, edad);
 });
