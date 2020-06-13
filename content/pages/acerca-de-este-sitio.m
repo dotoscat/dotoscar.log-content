e0deff85ec53d837021058177ea5c03d1823eaf1
dotoscat <dotoscat@gmail.com>
2020-06-13 20:57:07 +0200

    Anyadir vieja pagina con cambio de extension

---
+++ content/pages/acerca-de-este-sitio.m
@@ -0,0 +1,17 @@
+title: Acerca de este sitio
+date: 2019/7/7
+
+Este sitio fué hecho con [pelican](http://docs.getpelican.com/en/stable/), un generador de web estáticas, y el contenido lo escribo en archivos [markdown](https://www.markdownguide.org/), un lenguaje de formato. Para generar el contenido html a partir de markdown se hace uso de plantillas con [Jinja2](http://jinja.pocoo.org/). Puedes ver [mi tema](https://github.com/dotoscat/dotoscar.log-content/tree/master/mi-tema) para pelican.
+
+El diseño fué inspirado en una serie de webs satíricas sobre diseño y desarrollo web:
+
+* [https://www.motherfuckingwebsite.com/](https://www.motherfuckingwebiste.com/)
+* [https://bestmotherfucking.website/](https://bestmotherfucking.website/)
+* [https://thebestmotherfucking.website/](https://thebestmotherfucking.website/)
+* [https://everybootstrap.site/](https://everybootstrap.site/)
+
+Para CSS no usa bootstrap. El CSS es ["artesano"](https://github.com/dotoscat/dotoscar.log-content/tree/master/mi-tema/static/css/main.css) haciendo uso de tecnologías modernas como [flex](https://developer.mozilla.org/en-US/docs/Web/CSS/flex). Tampoco hace uso de jQuery o cualquier otro framework de JavaScript. Usa ["vanilla JavaScript"](http://vanilla-js.com/) para mejorar la web, o hacer algo divertido, pero nunca para mostrar contenido. Con todo esto las plantillas no están saturadas con etiquetas div para el estilo, sino que [está estructurada de forma semántica con HTML5](https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Using_HTML_sections_and_outlines) (esto es un artículo, esto es un cuerpo, esto es una sección, esto es una lista...).
+
+Primero empecé a estructurar las plantillas sin estilo y verlos en un navegador. Ya después apliqué el estilo.
+
+Por supuesto seguiré experimentando y mejorando este sitio.
